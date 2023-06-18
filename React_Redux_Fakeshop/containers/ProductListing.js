7d7cfb2b7d93496f680c4ed8416198bdea43515e
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-06-18 23:57:38 +0200

    Update ProductListing.js

--- React_Redux_Fakeshop/containers/ProductListing.js
+++ React_Redux_Fakeshop/containers/ProductListing.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useCallback, useMemo } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../redux/actions/productsActions";
+import ProductComponent from "./ProductComponent";
 
-const ProductListing = () => {
-    const products = useSelector((state) => state.products);
-    console.log(products);
+const ProductPage = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const dispatch = useDispatch();
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+    dispatch(setProducts(response.data));
+  };
 
-    return (
-        <div className="ui grid container">
-            <h1>ProductListing</h1>
-        </div>
-    );
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log("Products :", products);
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
 };
 
-export default ProductListing;
+export default ProductPage;
