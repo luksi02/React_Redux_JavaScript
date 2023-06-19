5dcaaece5db6216f61582e6d76673e7cab63d7c5
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-06-20 01:54:26 +0200

    Update productReducer.js

--- React_Redux_Fakeshop/redux/reducers/productReducer.js
+++ React_Redux_Fakeshop/redux/reducers/productReducer.js
@@ -1,21 +1,23 @@
 import {ActionTypes} from "../constants/action-types";
 
 const initialState = {
-    // products: []
-    products: [
-        {
-            id: 1,
-            title: "Luksi",
-            category: "Programmer",
-        }
-    ]
+    products: []   
 }
 
 export const productReducer = (state = initialState, {type, payload}) => {
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
-            return state;
+            return {...state, products : payload};
         default:
             return state;
     }
 };
+
+export const selectedProductReducer = (state ={}, {type, payload}) => {
+    switch (type) {
+        case ActionTypes.SELECTED_PRODUCT:
+            return {...state, ...payload};
+        default:
+            return state;
+    }
+}
