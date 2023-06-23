2d889ef2ce9085a7b604bc8ee9d5e0f10afcae66
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-06-23 23:55:21 +0200

    Update productReducer.js

--- React_Redux_Fakeshop/redux/reducers/productReducer.js
+++ React_Redux_Fakeshop/redux/reducers/productReducer.js
@@ -13,11 +13,14 @@ export const productReducer = (state = initialState, {type, payload}) => {
     }
 };
 
-export const selectedProductReducer = (state ={}, {type, payload}) => {
-    switch (type) {
-        case ActionTypes.SELECTED_PRODUCT:
-            return {...state, ...payload};
-        default:
-            return state;
-    }
-}
+export const selectedProductsReducer = (state = {}, { type, payload }) => {
+  console.log(type);
+  switch (type) {
+    case ActionTypes.SELECTED_PRODUCT:
+      return { ...state, ...payload };
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
+    default:
+      return state;
+  }
+};
